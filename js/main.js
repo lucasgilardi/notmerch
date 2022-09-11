dfcbe88d85f7812a80a888a550a555ce0e646e73
Lucas Gilardi <lcsgilardi@gmail.com>
2022-09-11 09:50:27 -0300

    Marquee hero agregada

--- js/main.js
+++ js/main.js
@@ -50,3 +50,22 @@ modalCloses.forEach((modalClose) =>{
         });
     });
 });
+
+//Marquee
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+//agregado
+const marqueeContentTwo = document.querySelector("ul.marquee-contentTwo");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
+
+//agregado
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContentTwo.appendChild(marqueeContentTwo.children[i].cloneNode(true));
+}
