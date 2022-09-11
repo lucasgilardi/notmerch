1cd36c55af04a79928953c942f4f5633015950cc
Lucas Gilardi <lcsgilardi@gmail.com>
2022-09-10 21:47:30 -0300

    GCarousel agregado

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Menu hamburguesa
 const hamburguer = document.querySelector(".hamburguer");
 const navMenu = document.querySelector(".nav");
 
@@ -10,3 +11,20 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamburguer.classList.remove("active");
     navMenu.classList.remove("active");
 }));
+
+//Carousel experiencia
+const grande = document.querySelector(".grande");
+const punto = document.querySelectorAll(".punto");
+
+punto.forEach ( (p, i) =>{
+    punto[i].addEventListener("click", () =>{
+        let posicion = i;
+        let operacion = posicion * -50;
+        grande.style.transform = `translateX(${ operacion }%)`
+
+        punto.forEach((p, i) => {
+            punto[i].classList.remove('activo');
+        })
+        punto[i].classList.add('activo');
+    })
+})
